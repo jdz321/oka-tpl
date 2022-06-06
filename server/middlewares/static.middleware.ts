7512645083b21e7f171760611ace732f3d2e0825
spark <jdz321@163.com>
2022-06-06 17:40:01 +0800

    feat: init ssr

--- server/middlewares/static.middleware.ts
+++ server/middlewares/static.middleware.ts
@@ -1,9 +1,19 @@
-import { proxy } from 'https://deno.land/x/oak@v10.5.1/mod.ts'
+import { proxy, Middleware } from 'https://deno.land/x/oak@v10.5.1/mod.ts'
 
-const staticMiddleware = () => {
-  return proxy('http://localhost:3000', {
-    // match
-  })
+const staticMiddleware = (): Middleware => {
+  const ENV = Deno.env.get('ENV')
+  const isDev = ENV === 'dev' || ENV === 'development'
+  if (isDev) {
+    return proxy('http://localhost:3000', {
+      // match
+    })
+  }
+  return async (context) => {
+    await context.send({
+      root: `${Deno.cwd()}/dist`,
+      index: "index.html",
+    })
+  }
 }
 
 export { staticMiddleware }
